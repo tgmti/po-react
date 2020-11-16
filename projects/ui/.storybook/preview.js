9fd3f849877309412b0f8a55d037faa6d62e5572
Thiago Mota <tgmspawn@gmail.com>
2020-11-16 14:05:19 -0300

    Melhorias no Layout do Storybook

--- projects/ui/.storybook/preview.js
+++ projects/ui/.storybook/preview.js
@@ -1,12 +1,12 @@
-import { addDecorator } from '@storybook/react';
-// import { docs } from '@storybook/addon-docs';
-import centered from '@storybook/addon-centered/react';
+import { addParameters } from '@storybook/client-api';
 
-// addDecorator(docs);
-addDecorator(centered);
+addParameters({
+  viewMode: 'docs',
+});
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
 
 import '@po-ui/style/css/po-theme-default.min.css'
+import './portal.css'
