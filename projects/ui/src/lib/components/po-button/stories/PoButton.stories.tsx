eefa2d2d9d94ccb37714e095b1000b3e4bdd18fd
Thiago Mota <tgmspawn@gmail.com>
2020-11-16 14:06:10 -0300

    Stories movidos para dentro do componente
    Stories do PoButton movidos para dentro da pasta samples no componente, seguindo a mesma linha do po-angular

--- projects/ui/src/lib/components/po-button/stories/PoButton.stories.tsx
+++ projects/ui/src/lib/components/po-button/stories/PoButton.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
 
-import PoButton, { PoButtonProps } from '../../../lib/components/po-button';
+import PoButton, { PoButtonProps } from '..';
 
 export default {
   title: 'Button/PoButtonComponent',
